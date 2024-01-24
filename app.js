b55e2f055088c679dd28c161155c709eb6b58583
JanoiBJR21 <apichat26870@gmail.com>
2024-01-25 02:00:11 +0700

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function toCelsius(fahrenheit) {
+    let value = (fahrenheit - 32) * 5 / 9
+    return value.toFixed(2) + "°C";
+}
+function display(element, value) {
+
+    document.getElementById(element).innerHTML = "<br>" + value + "</br>"
+}
+function toFahrenheit(celsius) {
+    let value = (celsius * 9) / 5 + 32
+    return value.toFixed(2) + "°F";
+}
+function toCelsiusProgram(value) {
+    alert(toCelsius(value))
+}
+function toFahrenheitProgram(value) {
+    alert(toFahrenheit(value))
+}
